e7fa31075b2571c16f009e107c2bb25c6e83e828
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-06-10 10:11:57 -0700

    Set the loading url from the loading_url env var from the configmap

--- vicedeployments.go
+++ vicedeployments.go
@@ -106,6 +106,7 @@ func (e *ExposerApp) viceProxyCommand(job *model.Job) []string {
 		"--ingress-url", e.IngressBaseURL,
 		"--analysis-header", e.AnalysisHeader,
 		"--access-header", e.AccessHeader,
+		"--loading-url", "$(loading_url)",
 	}
 
 	return output
@@ -116,9 +117,18 @@ func (e *ExposerApp) viceProxyCommand(job *model.Job) []string {
 func (e *ExposerApp) deploymentContainers(job *model.Job) []apiv1.Container {
 	return []apiv1.Container{
 		apiv1.Container{
-			Name:            viceProxyContainerName,
-			Image:           e.ViceProxyImage,
-			Command:         e.viceProxyCommand(job),
+			Name:    viceProxyContainerName,
+			Image:   e.ViceProxyImage,
+			Command: e.viceProxyCommand(job),
+			EnvFrom: []apiv1.EnvFromSource{
+				{
+					ConfigMapRef: &apiv1.ConfigMapEnvSource{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "vice-configs",
+						},
+					},
+				},
+			},
 			ImagePullPolicy: apiv1.PullPolicy(apiv1.PullAlways),
 			Ports: []apiv1.ContainerPort{
 				{
