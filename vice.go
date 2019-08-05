aa049ac363856cd5c5c10c5fce3856a317a0ba0f
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-08-05 13:45:48 -0700

    Add debug log to app-exposer

--- vice.go
+++ vice.go
@@ -476,6 +476,8 @@ func (e *ExposerApp) VICEPods(writer http.ResponseWriter, request *http.Request)
 		return
 	}
 
+	log.Debugf("%d pods found for external-id %s\n", len(podlist.Items), id)
+
 	for _, p := range podlist.Items {
 		returnedPods = append(returnedPods, retPod{name: p.Name})
 	}
